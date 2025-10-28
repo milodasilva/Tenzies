974d167fc20c12e11fdf9a11d28a63b8dc101972
Milo da Silva <milodasp@gmail.com>
2025-10-28 10:35:52 -0300

    Die component

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Die from "./Die";
+
+function App() {
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={10} />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Die.jsx
@@ -0,0 +1,6 @@
+export default function Die(props) {
+  console.log(props)
+  return (
+    <button>{props.value}</button>
+  )
+}
