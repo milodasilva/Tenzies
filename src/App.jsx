475d7f41fb406bf9e43fbdfe97708baaefaccbf1
Milo da Silva <milodasp@gmail.com>
2025-11-05 11:58:45 -0300

    Tenzies: Hold dice - part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,90 @@
 import Die from "./Die";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return Array.from({ length: 10 }, () => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: true,
+      id: nanoid(),
+    }));
   }
 
-  const diceElements = dice.map((item, index) => (
-    <Die key={index} value={item} />
+  const diceElements = dice.map((item) => (
+    <Die
+      key={item.id}
+      value={item.value}
+      isHeld={item.isHeld}
+      hold={() => hold(item.id)}
+    />
   ));
-
   const rollDice = () => {
     setDice(generateAllNewDice());
   };
 
+  function hold(id) {
+    console.log("ID " + id);
+  }
+
+  // Hold(props.id)
+
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-btn" onClick={rollDice}>Roll</button>
+      <button className="roll-btn" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
 
-export default App;
+// Constant       xxx ctermfg=253 guifg=#c678dd
+// String         xxx links to Constant
+
+// ysiw"     →    Word   →   "Word"
+// Visual Shift S '
+
+// Usando visual mode:
+// viw (seleciona a palavra)
+// S" (surround com aspas)
+
+// Trocar surround existente
+// cs"'      →   "Word"   →   'Word'
+// cs)(      →   (Word)   →   Word
+// cs"(      →   "Word"   →   (Word)
+
+// Delete
+// ds '
+
+// html
+// ysiw<tdiv>
+// → <div>Word</div>
+
+// =======================
+// hello
+// const obj = {
+//   value: Math.ceil(Math.random() * 6),
+//   isHeld: false,
+// }
+
+// const obj = Object.fromEntries(
+//   Array.from({ length: 10 }, (_, i) => [i, 0])
+// );
+//
+// console.log(obj);
+// // {0: 0, 1: 0, 2: 0, ..., 9: 0}
+
+// function generateAllNewDice() {
+//   return new Array(10)
+//     .fill(0)
+//     .map(() => ({
+//       value: Math.ceil(Math.random() * 6),
+//       isHeld: false,
+//       id: nanoid()
+//     }));
+// }
+
+//
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,12 @@
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
 
+  // console.log(props.Hold)
   return (
-    <button>{props.value}</button>
-  )
+    <button onClick={props.hold} style={styles}>
+      {props.value}
+    </button>
+  );
 }
