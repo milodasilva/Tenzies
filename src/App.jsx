e81dc06be4bdbfdbf251324a2855e5baa8ab35be
Milo da Silva <milodasp@gmail.com>
2025-11-19 15:55:38 -0300

    Tenzies: Accessibility Improvements - part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import Die from "./Die";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null);
 
   const gameWon =
     dice.every((die) => die.isHeld) &&
     dice.every((die) => die.value === dice[0].value);
 
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus();
+    }
+  }, [gameWon]);
+
   function generateAllNewDice() {
-    return Array.from({ length: 10 }, () => ({
+    return Array.from({ length: 3 }, () => ({
       value: Math.ceil(Math.random() * 6),
       isHeld: false, // agora começa desbloqueado
       id: nanoid(),
@@ -39,6 +46,7 @@ export default function App() {
       );
     } else {
       setDice(generateAllNewDice());
+      // buttonRef.current.focus()
     }
   }
 
@@ -60,9 +68,10 @@ export default function App() {
         )}
       </div>
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-btn" onClick={rollDice}>
+      <button ref={buttonRef} className="roll-btn" onClick={rollDice}>
         {gameWon ? "New Game" : "Roll"}
       </button>
+      {/*  <input ref={buttonRef} /> */}
     </main>
   );
 }
